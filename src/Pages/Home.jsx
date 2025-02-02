b234198c0221cf8440486ab88dbca6ba203048db
Dhanushka <pdhanushka856@gmail.com>
2025-02-02 11:24:29 +0530

    init

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero/Hero";
 import NavigationBar from "../components/NavigationBar/NavigationBar";
+import AboutSection from "../components/About/About";
 
 const Home = () => {
   return (
     <>
       <NavigationBar />
       <Hero />
+      <AboutSection />
     </>
   );
 };
